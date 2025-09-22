cf01273462166462b7f265f6687f861dc2c7eb6a
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2025-09-22 06:13:05 -0700

    Install GSAP

--- project/src/pages/Agence.jsx
+++ project/src/pages/Agence.jsx
@@ -3,30 +3,37 @@ import React from "react";
 const Agence = () => {
   return (
     <div>
-      <div className="h-[20vw] w-[15vw] absolute top-40 left-[30vw] rounded-4xl overflow-hidden">
-        <img className="w-full h-full object-cover" src="http://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg" alt="" />
-      </div>
+      <div className="section1">
+        <div className="h-[20vw] w-[15vw] absolute top-40 left-[30vw] rounded-4xl overflow-hidden">
+          <img
+            className="w-full h-full object-cover"
+            src="http://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg"
+            alt=""
+          />
+        </div>
 
-   
-    <div className="font-[font-2] relative">
-      <div className="mt-[52vh]">
-        <h1 className="text-[19vw] leading-[17vw] font-bold text-center">
-          SEVEN7Y <br />
-          TWO
-        </h1>
-      </div>
+        <div className="font-[font-2] relative">
+          <div className="mt-[52vh]">
+            <h1 className="text-[19vw] leading-[17vw] font-bold text-center">
+              SEVEN7Y <br />
+              TWO
+            </h1>
+          </div>
 
-      <div className="pl-[45%] mt-10">
-        <p className="text-5xl font-bold">
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We’re inquisitive and open-minded, and we make sure creativity crowds
-          out ego from every corner. A brand is a living thing, with values, a
-          personality and a story. If we ignore that, we can achieve short-term
-          success, but not influence that goes the distance. We bring that
-          perspective to every brand story we help tell.
-        </p>
+          <div className="pl-[45%] mt-10">
+            <p className="text-5xl font-bold">
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We’re
+              inquisitive and open-minded, and we make sure creativity crowds
+              out ego from every corner. A brand is a living thing, with values,
+              a personality and a story. If we ignore that, we can achieve
+              short-term success, but not influence that goes the distance. We
+              bring that perspective to every brand story we help tell.
+            </p>
+          </div>
+        </div>
       </div>
+      <div className="section2 h-screen"></div>
     </div>
-     </div>
   );
 };
 
