51c3b716b39d91efc27be8643d14305028231b96
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2025-09-23 05:12:06 -0700

    working on full screen navbar

---
+++ project/src/components/navigation/FullScreenNavigation.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const FullScreenNavigation = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <div>
+            <h1>Projects</h1>
+            <div>
+              <div>
+                <div>
+                  <h1>Pour tout voir</h1>
+                  <img src="" alt="" />
+                </div>
+                <div>
+                  <h1>Pour tout voir</h1>
+                  <img src="" alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FullScreenNavigation;
